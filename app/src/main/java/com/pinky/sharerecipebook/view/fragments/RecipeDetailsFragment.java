81c09e0a084d9f587311632eb0899216594beeee
aronchik <aaron.le94@gmail.com>
2021-10-08 14:51:03 +0300

    Successful merge with master. Branch is now updated

--- app/src/main/java/com/pinky/sharerecipebook/view/fragments/RecipeDetailsFragment.java
+++ app/src/main/java/com/pinky/sharerecipebook/view/fragments/RecipeDetailsFragment.java
@@ -1,9 +1,5 @@
 package com.pinky.sharerecipebook.view.fragments;
 
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.Intent;
-import android.content.IntentFilter;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -14,32 +10,26 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
-import androidx.localbroadcastmanager.content.LocalBroadcastManager;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager2.widget.ViewPager2;
 
-import com.android.volley.AuthFailureError;
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
 import com.astritveliu.boom.Boom;
 import com.bumptech.glide.Glide;
-import com.google.android.gms.tasks.Task;
+import com.google.android.material.tabs.TabLayout;
 import com.google.firebase.messaging.FirebaseMessaging;
 import com.pinky.sharerecipebook.R;
-import com.pinky.sharerecipebook.adapters.CommentAdapter;
-import com.pinky.sharerecipebook.models.Comment;
+import com.pinky.sharerecipebook.adapters.PagerAdapterDetails;
 import com.pinky.sharerecipebook.models.Recipe;
 import com.pinky.sharerecipebook.models.User;
+import com.pinky.sharerecipebook.view.fragments.ui.prepareAndIngredients;
+import com.pinky.sharerecipebook.view.fragments.ui.uiCommentsFragment;
 import com.pinky.sharerecipebook.viewmodels.RecipeDetailsViewModel;
 
-import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
@@ -47,48 +37,42 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
 
 public class RecipeDetailsFragment extends Fragment {
 
     private final RecipeDetailsFragment mthis = this;
     private TextView recipe_title;
-    private TextView recipe_ingredients;
-    private TextView recipe_preparation;
     private TextView recipe_details_likes_text;
     private TextView recipe_details_user_name_text;
-    private ImageView recipe_img_string;
+    private ImageView recipe_img_url_string;
     private ImageView recipe_details_image_like;
     private ImageView recipe_details_image_user;
-    private RecipeDetailsViewModel recipeDetailsViewModel;
     private Boolean likeRecipe;
     private User LoginUserGet;
+    private TabLayout tabLayout;
+    private ViewPager2 viewPager2;
+
+    private RecipeDetailsViewModel recipeDetailsViewModel;
 
     final String API_TOKEN_KEY = "AAAAnhV6-hM:APA91bGf-3U4CbbpRZWDOXa_jRLp4fmGCrj8C2qdWMF7q82umHfj5-aVsJI_jj_8mGFDbyh3v_dpg_9EuMIf4ePq0aiJ7isGVbE8eiO_kxgjwC2t_HCqipD3poyvSRfOuOE0LA-M5LXq";
     FirebaseMessaging messaging = FirebaseMessaging.getInstance();
-    BroadcastReceiver receiver;
-
-    private ArrayList<Comment> commentArrayList;
-    private CommentAdapter commentAdapter;
-
-    private Map<String, Comment> commentHashMap;
 
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
+        ActionBar actionBar = ((AppCompatActivity) getActivity()).getSupportActionBar();
+
+        if (actionBar != null) {
+            actionBar.hide();
+        }
+
         super.onCreate(savedInstanceState);
+
         likeRecipe = false;
         recipeDetailsViewModel = new ViewModelProvider(this).get(RecipeDetailsViewModel.class);
         recipeDetailsViewModel.init();
 
         //add all live? todo
-
-        commentArrayList = new ArrayList<>();
-
-        commentHashMap = new HashMap<>();
-
 
     }
 
@@ -96,17 +80,23 @@ public class RecipeDetailsFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 //        return inflater.inflate(R.layout.fragment_recipe_details_page, container, false);
+
+
         View view = inflater.inflate(R.layout.fragment_recipe_details_page, container, false);
 
         recipe_title = view.findViewById(R.id.frag_recipe_details_title_text);
         recipe_details_user_name_text = view.findViewById(R.id.frag_recipe_details_user_name_text);
         recipe_details_likes_text = view.findViewById(R.id.frag_recipe_details_likes_text);
 
-        recipe_ingredients = view.findViewById(R.id.fragment_show_recipe_ingredients);
-        recipe_preparation = view.findViewById(R.id.fragment_show_recipe_preparation);
-        recipe_img_string = view.findViewById(R.id.fragment_show_recipe_image);
+
+        recipe_img_url_string = view.findViewById(R.id.fragment_show_recipe_image);
         recipe_details_image_like = view.findViewById(R.id.frag_recipe_details_image_like);
         recipe_details_image_user = view.findViewById(R.id.frag_recipe_details_image_user);
+
+        tabLayout = view.findViewById(R.id.frag_recipe_details_tabLayout);
+        viewPager2 = view.findViewById(R.id.frag_recipe_details_view_Pager);
+
+        //tabLayout.setupWithViewPager(viewPager2);
 
 
         new Boom(recipe_details_image_like);
@@ -117,13 +107,10 @@ public class RecipeDetailsFragment extends Fragment {
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-
-
         super.onViewCreated(view, savedInstanceState);
 
         Recipe recipeGet = (Recipe) requireArguments().getSerializable("expandRecipe");
         LoginUserGet = (User) requireArguments().getSerializable("expandLoginUser");
-//        Log.d("UserGet", "onViewCreated: " + UserGet.getName());
 //        recipeDetailsViewModel.setRecipe(recipeGet); todo?
 
         recipeDetailsViewModel.getUserLiveData(recipeGet.getFirebaseUserIdMade()).getValue();
@@ -147,17 +134,11 @@ public class RecipeDetailsFragment extends Fragment {
                         });
 
         recipe_title.setText(recipeGet.getTitle());
-        recipe_ingredients.setText(recipeGet.getIngredients());
-        recipe_preparation.setText(recipeGet.getPreparation());
-        commentArrayList = recipeGet.getCommentArrayList();
-        commentHashMap = recipeGet.getCommentArrayListHashMap();
 
-        if (commentArrayList == null)
-            commentArrayList = new ArrayList<>();
 
         // set up img like
         recipe_details_likes_text.setText(String.valueOf(recipeGet.getRank()));
-        if (!LoginUserGet.getName().equals("Guest"))
+        if (!LoginUserGet.getName().equals("Guest")) // todo
             if (LoginUserGet.getFavoriteRecipe().contains(recipeGet.getRecipeId())) {
                 likeRecipe = true;
                 recipe_details_image_like.setImageResource(R.drawable.ic_baseline_favorite_48);
@@ -166,11 +147,10 @@ public class RecipeDetailsFragment extends Fragment {
         // load recipe img
         Glide.with(this)
                 .load(recipeGet.getImagePath())
-                //.thumbnail(1f)
 //                .placeholder(R.drawable.gallery)
                 .centerCrop()
                 .error(android.R.drawable.ic_dialog_info)
-                .into(recipe_img_string);
+                .into(recipe_img_url_string);
 
         // like recipe
         recipe_details_image_like.setOnClickListener(v -> {
@@ -199,7 +179,7 @@ public class RecipeDetailsFragment extends Fragment {
 
                 // change like in user
                 recipeDetailsViewModel.addIdLikeToUser(LoginUserGet.getFirebaseUserId(), LoginUserGet.getFavoriteRecipe());
-                // notification to user
+
                 sendLikeNotification(LoginUserGet.getName(),
                         "deLzdVdlR76RqieFtWI0ry:APA91bHY5nliOWp-j0ZK9PFWdVU0g2MKXDM6Ye6DF0s1OwF0gHd3oeCxINkJhhsnGwKPoRy8T-kbPSdG3ESMfXc7vCyS0_xJ9Sl8XRmMuC22fNJr_t6L7YEuvJKMR6ojP5SYC97qYP4L",
                         recipeGet.getTitle());
@@ -209,17 +189,44 @@ public class RecipeDetailsFragment extends Fragment {
             }
         });
 
-        RecyclerView recyclerView = view.findViewById(R.id.recycler_view_of_comments);
-        recyclerView.setHasFixedSize(true);
+        //  pager Adapter
+        FragmentManager fragmentManager = getParentFragmentManager();
 
-//        commentAdapter = new CommentAdapter(commentArrayList, getActivity());
-//        commentAdapter = new CommentAdapter(commentHashMap, getActivity());
-//        commentAdapter.setClicksListener(this);
+        PagerAdapterDetails pagerAdapter = new PagerAdapterDetails(fragmentManager, getLifecycle());
 
-        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
-        recyclerView.setAdapter(commentAdapter);
+        // adding fragments
+        pagerAdapter.addFragment(new prepareAndIngredients(recipeGet.getIngredients()));
+        pagerAdapter.addFragment(new prepareAndIngredients(recipeGet.getPreparation()));
+        pagerAdapter.addFragment(new uiCommentsFragment(recipeGet.getRecipeId(), recipeGet.getCommentArrayListHashMap(), LoginUserGet.getUserImagePath()));
+
+        viewPager2.setAdapter(pagerAdapter);
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager2.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+        // update tabLayout
+        viewPager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                tabLayout.selectTab(tabLayout.getTabAt(position));
+                super.onPageSelected(position);
+            }
+        });
     }
-
 
     public String sendLikeNotification (String SenderName, String ownerToken, String recipeName)  {
         Runnable runnable = new Runnable() {
