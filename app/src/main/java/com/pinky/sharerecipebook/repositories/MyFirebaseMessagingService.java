3bbb8adee3a8b9a9170f8d27e2334d9c060472d6
aronchik <aaron.le94@gmail.com>
2021-10-08 03:43:21 +0300

    Clicking notification now brings user to the app.

--- app/src/main/java/com/pinky/sharerecipebook/repositories/MyFirebaseMessagingService.java
+++ app/src/main/java/com/pinky/sharerecipebook/repositories/MyFirebaseMessagingService.java
@@ -3,6 +3,7 @@ package com.pinky.sharerecipebook.repositories;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Intent;
 import android.os.Build;
 import android.util.Log;
@@ -13,6 +14,7 @@ import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
+import com.pinky.sharerecipebook.view.MainActivity;
 
 
 public class MyFirebaseMessagingService extends FirebaseMessagingService{
@@ -36,17 +38,17 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService{
         // Check if message contains a data payload.
         if (remoteMessage.getData().size() > 0) {
             Log.d(TAG, "Message data payload: " + remoteMessage.getData());
-            Intent intent = new Intent("message received");
-            intent.putExtra("message", remoteMessage.getData().get("message"));
-            LocalBroadcastManager.getInstance(this).sendBroadcast(intent);
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
             NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
             Notification.Builder builder = new Notification.Builder(this);
 
+            // Pending intent to bring user to the app
+            PendingIntent pendingIntent = PendingIntent.getActivity(getApplicationContext(), 0, intent, 0);
 
             if(Build.VERSION.SDK_INT>=26){
                 NotificationChannel channel = new NotificationChannel("id_1", "name_1",NotificationManager.IMPORTANCE_HIGH);
                 manager.createNotificationChannel(channel);
-                builder.setChannelId("id_1");
+                builder.setChannelId("id_1").setContentIntent(pendingIntent);
             }
             builder.setContentTitle(remoteMessage.getData().get("title")).setContentText(remoteMessage.getData().get("message")).setSmallIcon(android.R.drawable.btn_star);
             manager.notify(1, builder.build());
